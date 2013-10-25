9b88d919c334a8c3b48eab752fd7e619358833f9
importre <importre@icloud.com>
2013-10-26 01:05:56 +0900

    단어 추가 시 하이라이트 바로 적용
    컨텍스트 메뉴를 통해 단어를 추가하면
    하아라이트를 다시 하도록 수

--- background.js
+++ background.js
@@ -7,45 +7,39 @@ function onClick(info, tab) {
         'text': word,
         'complete': DEFAULT_COMPLETE_VALUE
     };
-    chrome.storage.sync.set(
-        map, function () {
-            // Notify that we saved.
-        }
-    );
+    chrome.storage.sync.set(map, function () {
+        // Notify that we saved.
+        var request = {cmd: "refresh"};
+        chrome.tabs.sendMessage(tab.id, request, function (response) {
+            console.log(response);
+        });
+    });
 }
 
-chrome.contextMenus.create(
-    {
-        title: "add '%s' to paintover",
-        contexts: ["selection"],
-        onclick: onClick
-    }
-);
+chrome.contextMenus.create({
+    title: "add '%s' to paintover",
+    contexts: ["selection"],
+    onclick: onClick
+});
 
 chrome.extension.onRequest.addListener(
     function (request, sender, sendResponse) {
         chrome.pageAction.show(sender.tab.id);
 
         if (request.method == "storage" && request.key == "wordlist") {
-            chrome.storage.sync.get(
-                function (items) {
-                    sendResponse({data: items});
-                }
-            )
+            chrome.storage.sync.get(function (items) {
+                sendResponse({data: items});
+            })
         } else {
             sendResponse({});
         }
     }
 );
 
-chrome.pageAction.onClicked.addListener(
-    function (tab) {
-        chrome.tabs.create(
-            {
-                index: tab.index + 1,
-                url: "options.html"
-            }, function (tab) {
-            }
-        );
-    }
-);
+chrome.pageAction.onClicked.addListener(function (tab) {
+    chrome.tabs.create({
+        index: tab.index + 1,
+        url: "options.html"
+    }, function (tab) {
+    });
+});
