6fea477eb2be840b6afc2d8f073aec1579310e1c
JeadoKo <haibane84@gmail.com>
2013-10-27 11:10:03 +0900

    focus 관련 추가

--- contentscript.js
+++ contentscript.js
@@ -106,6 +106,10 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
         var response = {result: "ok"};
         sendResponse(response);
     }
+
+    if(request.cmd == "focus"){
+        focus();
+    }
 });
 
 function addDomModifiedEvent() {
@@ -154,4 +158,213 @@ function getRGBA(hex, a) {
     ];
 
     return "rgba(" + rgbaList.join(",") + ")";
+}
+
+
+
+function focus() {
+    console.log($("span.mark").get());
+    // Padding around the selection
+    var PADDING = 5;
+
+    // Opacity of the overlay
+    var OPACITY = 0.75;
+
+    // Key modifier that needs to be held down for overlay to appear
+    var MODIFIER = null;
+
+    // The opaque overlay canvas
+    var overlay,
+        overlayContext,
+        overlayAlpha = 0,
+
+    // Reference to the redraw animation so it can be cancelled
+    redrawAnimation,
+
+    // Currently selected region
+    selectedRegion = { left: 0, top: 0, right: 0, bottom: 0 },
+
+    // Currently cleared region
+    clearedRegion = { left: 0, top: 0, right: 0, bottom: 0 },
+
+    // Currently pressed down key modifiers
+    keyModifiers = { ctrl: false, shift: false, alt: false, cmd: false };
+
+    // Ensures that Fokus isn't initialized twice on the same page
+    window.__fokused = true;
+
+    overlay = document.createElement( 'canvas' );
+    overlayContext = overlay.getContext( '2d' );
+
+    // Place the canvas on top of everything
+    overlay.style.position = 'fixed';
+    overlay.style.left = 0;
+    overlay.style.top = 0;
+    overlay.style.zIndex = 2147483647;
+    overlay.style.pointerEvents = 'none';
+    overlay.style.background = 'transparent';
+
+    // document.addEventListener( 'mousedown', onMouseDown, false );
+
+    onWindowResize();
+    function onMouseDown( event ) {
+
+            updateSelection();
+
+    }
+
+    /**
+     * Steps through all selected nodes and updates the selected
+     * region (bounds of selection).
+     *
+     * @param {Boolean} immediate flags if selection should happen
+     * immediately, defaults to false which means the selection
+     * rect animates into place
+     */
+    function updateSelection( immediate ) {
+
+        // Default to negative space
+        var currentRegion = { left: Number.MAX_VALUE, top: Number.MAX_VALUE, right: 0, bottom: 0 };
+
+        var nodes = getSelectedNodes();
+
+        for( var i = 0, len = nodes.length; i < len; i++ ) {
+            var node = nodes[i];
+
+            // Select parents of text nodes that have contents
+            if( node.nodeName === '#text' && node.nodeValue.trim() ) {
+                node = node.parentNode;
+            }
+
+            // Fetch the screen coordinates for this element
+            var position = getScreenPosition( node );
+
+            var x = position.x,
+                y = position.y,
+                w = node.offsetWidth,
+                h = node.offsetHeight;
+
+            // 1. offsetLeft works
+            // 2. offsetWidth works
+            // 3. Element is larger than zero pixels
+            // 4. Element is not <br>
+            if( node && typeof x === 'number' && typeof w === 'number' && ( w > 0 || h > 0 ) && !node.nodeName.match( /^br$/gi ) ) {
+                currentRegion.left = Math.min( currentRegion.left, x );
+                currentRegion.top = Math.min( currentRegion.top, y );
+                currentRegion.right = Math.max( currentRegion.right, x + w );
+                currentRegion.bottom = Math.max( currentRegion.bottom, y + h );
+            }
+            console.log(currentRegion);
+        }
+
+        // Don't update selection if a modifier is specified but not
+        // pressed down, unless there's already a selected region
+        if( !MODIFIER || MODIFIER === 'none' || keyModifiers[ MODIFIER ] || hasSelection() ) {
+            selectedRegion = currentRegion;
+        }
+
+        // If flagged, update the cleared region immediately
+        if( immediate ) {
+            clearedRegion = selectedRegion;
+        }
+
+        // Start repainting if there is a selected region
+        if( hasSelection() ) {
+            redraw();
+        }
+
+    }
+
+    function hasSelection() {
+        return true;
+    }
+
+    function onWindowResize( event ) {
+        overlay.width = window.innerWidth;
+        overlay.height = window.innerHeight;
+    }
+    
+    function getSelectedNodes() {
+        return $("span.mark").get();
+    }
+
+    /**
+     * Redraws an animates the overlay.
+     */
+    function redraw() {
+
+        // Cache the response of this for re-use below
+        var _hasSelection = hasSelection();
+
+        // Reset to a solid (less opacity) overlay fill
+        overlayContext.clearRect( 0, 0, overlay.width, overlay.height );
+        overlayContext.fillStyle = 'rgba( 0, 0, 0, '+ overlayAlpha +' )';
+        overlayContext.fillRect( 0, 0, overlay.width, overlay.height );
+
+        if( _hasSelection ) {
+            if( overlayAlpha < 0.1 ) {
+                // Clear the selection instantly if we're just fading in
+                clearedRegion = selectedRegion;
+            }
+            else {
+                // Ease the cleared region towards the selected selection
+                clearedRegion.left += ( selectedRegion.left - clearedRegion.left ) * 0.18;
+                clearedRegion.top += ( selectedRegion.top - clearedRegion.top ) * 0.18;
+                clearedRegion.right += ( selectedRegion.right - clearedRegion.right ) * 0.18;
+                clearedRegion.bottom += ( selectedRegion.bottom - clearedRegion.bottom ) * 0.18;
+            }
+        }
+
+        // Cut out the cleared region
+        overlayContext.clearRect(
+            clearedRegion.left - window.scrollX - PADDING,
+            clearedRegion.top - window.scrollY - PADDING,
+            ( clearedRegion.right - clearedRegion.left ) + ( PADDING * 2 ),
+            ( clearedRegion.bottom - clearedRegion.top ) + ( PADDING * 2 )
+        );
+
+        // Fade in if there's a valid selection...
+        if( _hasSelection ) {
+            overlayAlpha += ( OPACITY - overlayAlpha ) * 0.08;
+        }
+        // ... otherwise fade out
+        else {
+            overlayAlpha = Math.max( ( overlayAlpha * 0.85 ) - 0.02, 0 );
+        }
+
+        // Ensure there is no overlap
+        cancelAnimationFrame( redrawAnimation );
+
+        // Continue so long as there is content selected or we are fading out
+        if( _hasSelection || overlayAlpha > 0 ) {
+            // Append the overlay if it isn't already in the DOM
+            if( !overlay.parentNode ) document.body.appendChild( overlay );
+
+            // Stage a new animation frame
+        }
+        else {
+            document.body.removeChild( overlay );
+        }
+
+    }
+
+        /**
+     * Gets the x/y screen position of the target node, source:
+     * http://www.quirksmode.org/js/findpos.html
+     */
+    function getScreenPosition( node ) {
+        var x = document.documentElement.offsetLeft,
+            y = document.documentElement.offsetTop;
+
+        if ( node.offsetParent ) {
+            do {
+                x += node.offsetLeft;
+                y += node.offsetTop;
+            } while ( node = node.offsetParent );
+        }
+
+        return { x: x, y: y };
+    }
+    updateSelection();
+
 }
